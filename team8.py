f9aa05623662d91e70995d42d335960366c4c35d
Tristan <234285@shakopeeschools.org>
2019-01-23 10:34:58 -0600

    Revert "Update team8.py"
    This reverts commit 8b695dd65a23e27eaedff66b0fed9bb8b3c66f44.

--- team8.py
+++ team8.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Tristan Jenks team' # Only 10 chars displayed.
-strategy_name = 'Opperation look back in their history and conced alot'
+strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
