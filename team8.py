53a49a499bb5c7ee97a30f7c60711aeec6819934
Tristan <234285@shakopeeschools.org>
2019-01-23 10:36:11 -0600

    Update team8.py
    strat

--- team8.py
+++ team8.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Tristan Jenks team' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'Operation look back into their history and caloude alot'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
